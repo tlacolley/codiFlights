cded63c98e35921c3a6879152ce8ac696b98096c
tlacolley <thomaslacolley@gmail.com>
2018-08-21 13:58:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" type="css/navbar-style.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+        <title>CodiFlights</title>
+    </head>
+    <body>
+        <?php $db = new PDO('mysql:host=localhost;dbname=codi-flights', 'admin', 'plop'); ?>
+        <h1>Codi Flights</h1>
+    </body>
+</html>
