b7276c821fa8e5f8c70e0b65a697bedae90f75aa
tlacolley <thomaslacolley@gmail.com>
2018-08-22 09:03:12 +0200

    Add diff date begining select city

--- index.php
+++ index.php
@@ -9,7 +9,72 @@
         <title>CodiFlights</title>
     </head>
     <body>
-        <?php $db = new PDO('mysql:host=localhost;dbname=codi-flights', 'admin', 'plop'); ?>
+        <?php
+        require "log.php";
+        $db = new PDO('mysql:host=localhost;dbname=codi-flights', $identifiant, $password);
+        $airports = $db->query('SELECT * FROM airport')->fetchAll();
+        $flys= $db->query('SELECT flight.*, arrival.city AS arrival_city , depart.city AS depart_city
+            FROM flight
+            LEFT JOIN airport AS depart
+            ON flight.airport_from = depart.code
+            LEFT JOIN airport AS arrival
+            ON flight.airport_to = arrival.code
+            ORDER BY id ')->fetchAll();
+        ?>
         <h1>Codi Flights</h1>
+
+        <form class="" action="index.php" method="get">
+            <input type="search" name="searchBar" value="">
+            <select class="" name="cityselect">
+                <?php foreach ($airports as $cityDepart):?>
+                    <option value=""><?php echo $cityDepart["city"] ?></option>
+                <?php endforeach ?>
+                <input type="submit" name="btnSubmit" value="search">
+            </select>
+        </form>
+        <?php
+        if(!empty($_get["search"])){
+            $search = $_get["search"];
+            $dbSearch = "SELECT * FROM flight WHERE depart_city LIKE '%".$search."%'";
+
+        }
+
+         ?>
+
+            <table>
+                    <tr>
+                      <td>Code de Vol</td>
+                      <td>Ville de départ</td>
+                      <td>Date de départ</td>
+                      <td>Ville d'arrivé</td>
+                      <td>Date d'arrivé</td>
+                      <td>Duree du vol </td>
+                    </tr>
+                    <?php foreach ($flys as $fly):?>
+                    <tr>
+                      <td><?php echo $fly["code"]?></td>
+                      <td><?php echo $fly["depart_city"]." ". $fly["airport_from"] ?></td>
+                      <td><?php echo $fly['departure_date']; ?></td>
+                      <td><?php echo $fly["arrival_city"]." ". $fly["airport_to"] ?></td>
+                      <td><?php echo $fly["arrival_date"] ?></td>
+                      <td>
+                      <?php
+                      $dateDepart = new DateTime($fly["departure_date"]);
+                      $dateArrival = new DateTime($fly["arrival_date"]);
+                      // print_r(date_diff($dateDepart,$dateArrival));
+                      $dureeVol = $dateArrival->diff($dateDepart)->format(" %h hours and %i minuts");
+                      echo $dureeVol;
+                      ?>
+                       </td>
+                    </tr>
+
+            <tr>
+
+
+        <?php endforeach ?>
+
+
+    </table>
+
     </body>
 </html>
